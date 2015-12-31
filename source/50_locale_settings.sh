dab0734b70390eb1d1ae39003506b039eae7b143
Johan Tique <johan.tique@analisistem.com>
2015-12-30 19:22:58 -0500

    remove annoying mesage from locales and remove the pebble script

---
+++ source/50_locale_settings.sh
@@ -0,0 +1,13 @@
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+export LC_PAPER=es_CO.UTF-8
+export LC_ADDRESS=es_CO.UTF-8
+export LC_MONETARY=es_CO.UTF-8
+export LC_NUMERIC=es_CO.UTF-8
+export LC_TELEPHONE=es_CO.UTF-8
+export LC_IDENTIFICATION=es_CO.UTF-8
+export LC_MEASUREMENT=es_CO.UTF-8
+export LC_CTYPE=es_ES.UTF-8
+export LC_TIME=es_CO.UTF-8
+export LC_NAME=es_CO.UTF-8
+export LANG=en_US.UTF-8
